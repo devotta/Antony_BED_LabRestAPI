275c643ea3144e6534a51685ac8271dce3bd1eea
Antony Roy <47647718+devotta@users.noreply.github.com>
2023-05-12 12:27:13 +0500

    Add files via upload

---
+++ StudentEventReg/src/test/java/com/gl/studenteventreg/StudentEventReg/StudentEventRegApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gl.studenteventreg.StudentEventReg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentEventRegApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
